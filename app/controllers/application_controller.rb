10f9ba2bee048f2788c8205f57293be8a7f6dde2
samiylo <skryshu@gmail.com>
2020-05-12 01:51:04 -0500

    added index erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ class ApplicationController < Sinatra::Base
         set :public_folder, 'public'
         set :views, 'app/views'
         enable :sessions
-        set :session_secret, "mysticaltasks"
+        set :session_secret, "gamesecret"
+    end
+
+    get '/' do 
+        erb :index
     end
 
 
