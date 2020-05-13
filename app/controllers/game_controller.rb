32686eea4b2949e29c1df960b5891cbf153ddd0e
samiylo <skryshu@gmail.com>
2020-05-13 14:54:17 -0500

    Update

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,4 +7,55 @@ class GamesController < ApplicationController
         erb :'games/show'
     end
 
+    get '/games/new' do
+        redirect_if_not_logged_in
+        @user = current_user
+        @lists = List.all
+        erb :'/games/new'
+    end
+    
+    post '/games/new' do
+        Game.create(:name => params[:name], :list_id => params[:list_id])
+        redirect '/games'
+        end
+    
+    
+    get '/games/new/:id' do
+        redirect_if_not_logged_in
+        @list = List.find_by_id(params[:id])
+        erb :'/games/new_on_list'
+    end
+    
+    post '/games/new/:id' do
+        @list = List.find_by_id(params[:id])
+        Game.create(:name => params[:name], :list_id => params[:id])
+        redirect '/games'
+    end
+    
+    
+    get '/games/:id/edit' do
+        @game = Game.find_by_id(params[:id])
+        erb :'/games/edit'
+    end
+    
+    patch '/games/:id' do
+        @game = Game.find_by_id(params[:id])
+        @game.name = params[:name]
+        @game.save
+        redirect '/games'
+    end
+    
+    
+    get '/games/:id/delete' do 
+        @game = Game.find_by_id(params[:id])
+        erb :'/games/delete'
+    end
+    
+    delete '/games/:id' do 
+        @game = Game.find_by_id(params[:id])
+        @game.destroy
+        redirect '/games'
+    end
+    
+
 end
--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -4,4 +4,35 @@ class ListsController < ApplicationController
         redirect_if_not_logged_in
         erb :'/lists/new'
     end
+
+    post '/lists/new' do
+        @user = current_user
+        list = @user.lists.create(:name => params[:name])
+        game = list.games.create(:name => params[:games][:name])
+        redirect '/games'
+    end
+    
+    get '/lists/:id/edit' do
+        redirect_if_not_logged_in
+        @list = List.find_by_id(params[:id])
+        erb :'lists/edit'
+    end
+    
+    patch '/lists/:id' do 
+        @list = List.find_by_id(params[:id])
+        @list.name = params[:name]
+        @list.save
+        redirect '/games'
+    end
+    
+    get '/lists/:id/delete' do
+        @list = List.find_by_id(params[:id])
+        erb :'lists/delete'
+    end
+    
+    delete '/lists/:id' do 
+        @list = List.find_by_id(params[:id])
+        @list.destroy
+        redirect '/games'
+    end
 end
